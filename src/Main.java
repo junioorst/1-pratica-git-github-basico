8c019cb46249080e124ac497acd7a3cdaa766ef4
Júnior Stahl <juniorstahl@gmail.com>
2025-03-14 18:51:49 -0300

    criacao array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        ArrayList <Integer> array = new ArrayList<>(10);
+        array.add(7);
+        array.add(2);
+        array.add(23);
+        array.add(45);
+        array.add(9);
     }
+
+    // Método que retorna quantas ocorrências de um elemento estão na lista
+
+
+    // c. Método que retorna uma lista de elementos repetidos de l
 }
